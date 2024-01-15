529757fdd0c67b2dbadfe8f8390ad8914190b4be
Tokiosman80F <tokiosman96@gmail.com>
2024-01-15 14:22:03 +0600

    created GET api route for getting all toys data

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
+require("dotenv").config();
+
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 const cors = require("cors");
 
+console.log("user => ", process.env.BUCKET);
+console.log("user pass=> ", process.env.BUCKET_KEY);
+
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri = `mongodb+srv://${process.env.BUCKET}:${process.env.BUCKET_KEY}@cluster0.lyiobzh.mongodb.net/?retryWrites=true&w=majority`;
 
-require("dotenv").config();
 app.use(cors());
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("World say hello to MiniTeen ");
 });
 
 // mongo db
@@ -27,6 +31,23 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const toys = client.db("miniTinDB").collection("toysCollection");
+
+    // ---getting all data
+    app.get("/all-toys", async (req, res) => {
+      const result = await toys.find().toArray();
+      res.send(result);
+    });
+
+    // ---posting toy data
+    app.post("/add-toy",async (req,res)=>{
+      const body=req.body
+      console.log("the data",body);
+      const result = await toys.insertOne(body)
+      res.send(result)
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
